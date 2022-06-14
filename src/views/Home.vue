286dde59178d615774d218754f88f0d10ffb56a7
928979883 <928979883@qq.com>
2022-06-14 18:23:35 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template lang="">
+    <div>
+        我是首页
+    </div>
+</template>
+<script>
+export default {
+    name:'Home',
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+<style lang="">
+    
+</style>
